d7a4f1584de376787e17e24b78f66f948862d80f
todd-bowers <toddbows@gmail.com>
2022-08-01 16:11:42 -0400

    StringDuplicateDeleter passes all tests

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.looplabs;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +14,31 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        ArrayList<String> removed = new ArrayList<>();
+        for (String string : array) {
+            if (counter(string) < maxNumberOfDuplications) {
+                removed.add(string);
+            }
+        }
+        return removed.toArray(new String[0]);
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        ArrayList<String> removed = new ArrayList<>();
+        for (String string : array) {
+            if (counter(string) != exactNumberOfDuplications) {
+                removed.add(string);
+            }
+        }
+        return removed.toArray(new String[0]);
+    }
+
+    public int counter(String string) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(string)) count++;
+        }
+        return count;
     }
 }
